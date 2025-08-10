c7cfd0240a3ac19cb3aab974167586d1d4fd2fee
nicholasbay <bay.yong.wei@gmail.com>
2025-08-10 12:54:29 +0000

    Add ugly code

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-def add(x, y):
-    return x + y
+def add(x,y):
+    return x+y
 
 
-input1, input2 = 1, 2
-print(f"The sum of {input1} and {input2} is {add(input1, input2)}")
+input1, input2 = 1, 2;print(f"The sum of {input1} and {input2} is {add(input1, input2)}")
